4c23adc68863f5fa8ca7df30f4c1737ea0451f51
Cyber_Infernox <sayonkar@gmail.com>
2023-06-01 08:39:22 +0530

    Tailwind Init

--- Client/my-app/src/App.js
+++ Client/my-app/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">WELCOME TO SocialIQu</div>;
+  return <div className="App bg-slate-500">WELCOME TO SocialIQu</div>;
 }
 
 export default App;
